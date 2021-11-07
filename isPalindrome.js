e09926140fb7bf3ea5cddb5ec7a554b915afa2de
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-07 17:39:33 +0300

    task

---
+++ isPalindrome.js
@@ -0,0 +1,9 @@
+function isPalindrome(x) {
+  for (let i = 0; i < x.length / 2; i++) {
+    if (x[i].toLowerCase() !== x[x.length - 1 - i].toLowerCase()) {
+      return false;
+    }
+    return true;
+  }
+  return true;
+}

808043ff64bf8d8789e31a030e06805516f727b3
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-15 18:44:41 +0300

    t

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,9 +1,8 @@
 function isPalindrome(x) {
-  for (let i = 0; i < (x.length / 2); i++) {
-    if (x[i].toLowerCase() !== x[x.length - 1 - i].toLowerCase()) {
-      return false;
+    for(let i = 0 ; i<(x.length/2) ; i++  ){
+      if(x[i].toLowerCase() !== x[x.length-1 -i].toLowerCase()){
+        return false
+      }
     }
-    return true;
-  }
-  return true;
-}
+       return true 
+   }
